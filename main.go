3bf1e1687f22434ed17e8dc1c6366f678c04bb81
Jozenias Antônio <fayrghos@proton.me>
2026-06-09 15:05:13 -0300

    Conversão do projeto para go com raylib

---
+++ main.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"runtime"
+
+	rl "github.com/gen2brain/raylib-go/raylib"
+)
+
+func init() {
+	runtime.LockOSThread()
+}
+
+func main() {
+	rl.InitWindow(1366, 768, "OSMV")
+	rl.SetTargetFPS(60)
+	defer rl.CloseWindow()
+
+	for !rl.WindowShouldClose() {
+		rl.BeginDrawing()
+
+		rl.ClearBackground(rl.Color{R: 29, G: 29, B: 32, A: 255})
+		rl.DrawText("Projetistas Software 3000", 800, 600, 30, rl.White)
+
+		rl.EndDrawing()
+	}
+}
