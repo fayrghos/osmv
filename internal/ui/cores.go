afd35cecb04f8eabe5cc4afb430a977ba3995eb3
Jozenias Antônio <fayrghos@proton.me>
2026-06-10 15:04:43 -0300

    Adição de botões de texto experimentais

---
+++ internal/ui/cores.go
@@ -0,0 +1,11 @@
+package ui
+
+import "image/color"
+
+var (
+	CorFundo     = color.RGBA{R: 29, G: 29, B: 32, A: 255}
+	CorPrincipal = color.RGBA{R: 52, G: 52, B: 53, A: 255}
+
+	CorSelecao  = color.RGBA{R: 222, G: 221, B: 218, A: 255}
+	CorDestaque = color.RGBA{R: 154, G: 153, B: 150, A: 255}
+)
--- main.go
+++ main.go
@@ -1,17 +1,39 @@
 package main
 
-import rl "github.com/gen2brain/raylib-go/raylib"
+import (
+	"github.com/fayrghos/osmv/internal/ui"
+	rl "github.com/gen2brain/raylib-go/raylib"
+)
 
 func main() {
 	rl.InitWindow(1366, 768, "OSMV")
 	rl.SetTargetFPS(60)
 	defer rl.CloseWindow()
 
+	fonte := rl.LoadFont("assets/fontes/LiberationSans.ttf")
+
+	botoes := make(ui.GrupoBotoes, 0, 10)
+	botoes = append(botoes, ui.Botao{
+		Pos:     rl.Vector2{X: 200, Y: 200},
+		Tamanho: rl.Vector2{X: 200, Y: 50},
+		Fonte:   &fonte,
+	})
+
+	botoes = append(botoes, ui.Botao{
+		Pos:     rl.Vector2{X: 200, Y: 300},
+		Tamanho: rl.Vector2{X: 200, Y: 50},
+		Fonte:   &fonte,
+	})
+
 	for !rl.WindowShouldClose() {
+		botoes.AtualizarTodos()
+
 		rl.BeginDrawing()
 
-		rl.ClearBackground(rl.Color{R: 29, G: 29, B: 32, A: 255})
+		rl.ClearBackground(ui.CorFundo)
 		rl.DrawText("Projetistas Software 3000", 800, 600, 30, rl.White)
+
+		botoes.DesenharTodos()
 
 		rl.EndDrawing()
 	}
