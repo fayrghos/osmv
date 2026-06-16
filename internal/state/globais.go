81c625e3983ed13314be531a10711cfa7e5318a3
viteihehe <victormanuelfernandesanacleto@gmail.com>
2026-06-16 17:46:58 -0300

    Update

--- internal/state/globais.go
+++ internal/state/globais.go
@@ -37,6 +37,8 @@ type Globais struct {
 	BotaoContinuar ui.Botao
 	BotaoPasso     ui.Botao
 	BotaoVoltar    ui.Botao
+
+	Quadros [][]int
 }
 
 // Essa função roda algo exatamente uma vez quando uma tela inicia
